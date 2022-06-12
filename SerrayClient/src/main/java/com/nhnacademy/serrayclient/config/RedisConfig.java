6e7a671cdc63b93ab65dd78b1d62157819e47bc1
psw4887 <83569822+psw4887@users.noreply.github.com>
2022-06-12 13:06:26 +0900

    commit;

--- SerrayClient/src/main/java/com/nhnacademy/serrayclient/config/RedisConfig.java
+++ SerrayClient/src/main/java/com/nhnacademy/serrayclient/config/RedisConfig.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.BeanClassLoaderAware;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
