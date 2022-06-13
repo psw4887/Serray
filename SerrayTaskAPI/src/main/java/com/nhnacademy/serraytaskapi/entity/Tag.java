0e4512db45fd3d6f534c8d62a92ce8f835b4b86d
psw4887 <83569822+psw4887@users.noreply.github.com>
2022-06-14 08:02:17 +0900

    소나큐브 테스트 및 인텔리제이 테스트코드 커버리지 추가

--- SerrayTaskAPI/src/main/java/com/nhnacademy/serraytaskapi/entity/Tag.java
+++ SerrayTaskAPI/src/main/java/com/nhnacademy/serraytaskapi/entity/Tag.java
@@ -7,7 +7,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.MapsId;
 import javax.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
